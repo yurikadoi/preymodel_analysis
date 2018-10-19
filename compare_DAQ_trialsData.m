215bb09162d99ec0e6431c192ad0282c354b388b
Yurika DOI <kuroshibashion@gmail.com>
2018-10-19 12:11:03 -0400

    added some more

--- compare_DAQ_trialsData.m
+++ compare_DAQ_trialsData.m
@@ -1,14 +1,16 @@
-load(['Trials',num2str(sessionDate),'.mat'])
+%%
+%load(['Trials',num2str(sessionDate),'.mat'])
+%%
 %before 10/10 when the first trials data (matfile) is garbage and the last trial is
 %not saved in the trials data (matfile)
 new_preyData_fromDAQ = preyData_fromDAQ;
 new_preyData_fromDAQ(:,4) = preyData_fromDAQ(:,4)/1000;
 new_preyData_fromDAQ(:,5) = preyData_fromDAQ(:,5)/1000;
 
-%new_preyData_fromDAQ = new_preyData_fromDAQ(1:end-1,:);
+new_preyData_fromDAQ = new_preyData_fromDAQ(1:end,:);
 
 new_trialsData = trials;
-%new_trialsData = new_trialsData(2:end,:);
+new_trialsData = new_trialsData(1:end,:);
 %%
 for i =2:3
     if new_preyData_fromDAQ(:,i)==new_trialsData(:,i)
@@ -24,7 +26,7 @@ else
     disp('discrepancy')
 end
 %%
-if new_preyData_fromDAQ(:,5) - 0.1 < new_trialsData(:,5) & new_preyData_fromDAQ(:,5) + 0.1 > new_trialsData(:,5)
+if new_preyData_fromDAQ(:,5) - 0.2 < new_trialsData(:,5) & new_preyData_fromDAQ(:,5) + 0.2 > new_trialsData(:,5)
     disp('no discrepancy')
 else
     disp('discrepancy')
