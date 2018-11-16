e325abc353a005054b6567095fa87f59378b452c
Yurika DOI <kuroshibashion@gmail.com>
2018-11-16 18:44:28 -0500

    added some comments

--- compare_DAQ_trialsData.m
+++ compare_DAQ_trialsData.m
@@ -1,11 +1,12 @@
 %%
-%load(['Trials',num2str(sessionDate),'.mat'])
+load(['Trials',num2str(sessionDate),'.mat'])
 %%
 %before 10/10 when the first trials data (matfile) is garbage and the last trial is
 %not saved in the trials data (matfile)
 new_preyData_fromDAQ = preyData_fromDAQ;
 new_preyData_fromDAQ(:,4) = preyData_fromDAQ(:,4)/1000;
 new_preyData_fromDAQ(:,5) = preyData_fromDAQ(:,5)/1000;
+new_preyData_fromDAQ(:,6) = preyData_fromDAQ(:,6)/1000;
 
 new_preyData_fromDAQ = new_preyData_fromDAQ(1:end,:);
 
@@ -20,7 +21,7 @@ for i =2:3
     end
 end
 %%
-if new_preyData_fromDAQ(:,4) - 0.05 < new_trialsData(:,4) & new_preyData_fromDAQ(:,4) + 0.05 > new_trialsData(:,4)
+if new_preyData_fromDAQ(:,4) - 0.1 < new_trialsData(:,4) & new_preyData_fromDAQ(:,4) + 0.1 > new_trialsData(:,4)
     disp('no discrepancy')
 else
     disp('discrepancy')
@@ -31,3 +32,14 @@ if new_preyData_fromDAQ(:,5) - 0.2 < new_trialsData(:,5) & new_preyData_fromDAQ(
 else
     disp('discrepancy')
 end
+%%
+
+if new_preyData_fromDAQ(:,6) - 0.2 < new_trialsData(:,6) & new_preyData_fromDAQ(:,6) + 0.2 > new_trialsData(:,6)
+    disp('no discrepancy')
+else
+    disp('discrepancy')
+end
+%%
+%{
+new_preyData_fromDAQ(:,6) - new_trialsData(:,6)
+%}
